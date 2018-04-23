2288681ed3ccd4265bd9af45473974a99ba44de3
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-04-23 19:09:12 -0300

    Rename default migration files

---
+++ src/migrations/0-initial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  /*
+   * This migration file is utilized to create the SequelizeMeta table
+   */
+  up: (queryInterface, Sequelize) => { },
+  down: (queryInterface, Sequelize) => { }
+};
